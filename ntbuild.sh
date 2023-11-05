9929d55d27fdffc7ac35677cebfba069dde82225
Eric Gallager <egall@gwmail.gwu.edu>
2023-11-05 10:48:39 -0500

    Update ntbuild.sh
    looks like the csproj doesn't work...

--- ntbuild.sh
+++ ntbuild.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 if test -e assets/RagePixel/RagePixelMonoDevelop/RagePixelMonoDevelop.csproj && test -x "$(which msbuild)"; then \
-	cd assets/RagePixel/RagePixelMonoDevelop && msbuild RagePixelMonoDevelop.csproj; \
+	cd assets/RagePixel/RagePixelMonoDevelop || exit; \
+	msbuild RagePixelMonoDevelop.csproj || msbuild Packages.mdproj; \
 fi
